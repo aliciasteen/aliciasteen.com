c541f389e715c26db5311c7a11664a2af739b95f
Alicia Steen <aliciasteen@users.noreply.github.com>
2019-03-01 08:53:24 +0000

    Update .travis.yml

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
+echo "Install terraform"
+curl -fSL "https://releases.hashicorp.com/terraform/0.11.11/terraform_0.11.11_linux_amd64.zip" -o terraform.zip
+sudo unzip terraform.zip -d /opt/terraform
+sudo ln -s /opt/terraform/terraform /usr/bin/terraform
+rm -f terraform.zip
+
 echo "Initalizing terraform"
 terraform init -input=false
 echo "Applying terraform"
